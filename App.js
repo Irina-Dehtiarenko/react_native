0f9df5f68bb8795c633a6220e2bb8860d40a0a79
Dehtiarenko <3119160@TEB-GDANSK.PL>
2022-06-12 10:39:26 +0200

    notatki dla rónych imputów

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ import {
   TextInput,
   Button,
   TouchableOpacity,
+  TouchableHighlight,
+  Pressable,
+  Alert,
+  ToastAndroid,
 } from 'react-native';
 
 const App = () => {
@@ -14,6 +18,18 @@ const App = () => {
   const [submit, setSubmit] = useState(false)
 
   const onPressHandler = () => {
+    if (name.length > 3) {
+      setSubmit(!submit);
+    }
+    else {
+      /*  Alert.alert('Warning', 'The name must be longer than 3 characters')
+       //Raki sobie alert, który informuje o warunkach */
+
+      ToastAndroid.show('The must be longer than 3 charakters', ToastAndroid.LONG)
+      //albo SHORT, zalży jak długo to okienko powinno się wyświetlać
+      //alert  na dole małe
+
+    }
     console.log(submit)
     setSubmit(!submit);
   }
@@ -35,12 +51,8 @@ const App = () => {
       //secureTextEntry //hasła
       />
 
-      <TouchableOpacity style={styles.button}
-        onPress={onPressHandler}>
-        <Text style={styles.text}>
-          {submit ? 'Crear' : 'Submit'}
-        </Text>
-      </TouchableOpacity>
+
+
 
       {/* <Button
         title={submit ? 'Clear' : 'Submit'}
@@ -48,6 +60,55 @@ const App = () => {
         color='#00f'
       //disabled={submit}//przycisk niedostępny
       /> */}
+
+
+
+
+      {/*  <TouchableOpacity style={styles.button}//zamiast przycisku
+        onPress={onPressHandler}
+        activeOpacity={0.1}>{//stopień przezroczystości }
+        <Text style={styles.text}>
+          {submit ? 'Crear' : 'Submit'}
+        </Text>
+      </TouchableOpacity>
+
+
+ */}
+
+
+      {/* 
+      <TouchableHighlight
+        style={styles.button}//zamiast przycisku
+        onPress={onPressHandler}
+        underlayColor={'#ddd'}>
+        {//zamienia kolor w momencie naciskania}
+        }
+        <Text style={styles.text}>
+          {submit ? 'Crear' : 'Submit'}
+        </Text>
+      </TouchableHighlight>
+ */}
+
+
+
+      <Pressable
+        style={({ pressed }) => [
+          { backgroundColor: pressed ? '#ddd' : '#0f0' }, styles.button
+        ]}
+        onLongPress={onPressHandler}
+        // Płynny przycisk
+        delayLongPress={300}
+        //prędkość naciskania
+        hitSlop={{ top: 10, bottom: 10, right: 10, left: 10 }}
+      //rozszerza kliknięcie poza ramki buttona
+
+
+      >
+        <Text style={styles.text}>
+          {submit ? 'Crear' : 'Submit'}
+        </Text>
+      </Pressable>
+
       {
         submit ? <Text style={styles.text}>
           You are registreted as {name}</Text> : null
@@ -68,7 +129,7 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   input: {
-    margin: 10,
+    marginBottom: 10,
     width: 200,
     borderWidth: 1,
     borderColor: '#555',
@@ -76,6 +137,13 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 20,
   },
+  button: {
+    width: 150,
+    height: 50,
+    alignItems: 'center',
+
+
+  }
 
 });
 
